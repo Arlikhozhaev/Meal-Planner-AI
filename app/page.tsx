b92b782b27e86161e246ec01053d50a94e0ea7d9
JAYDEN <jayxu231@gmail.com>
2024-09-06 19:22:19 -0700

    small reformatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import { Vcomponent } from "@/components/component/vcomponent";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Home() {
-  return <ClerkProvider> <Vcomponent /></ClerkProvider>
-}
+  return (
+  <ClerkProvider> 
+    <Vcomponent />
+  </ClerkProvider>
+  );
+};
